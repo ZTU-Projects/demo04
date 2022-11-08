fb14004e4221520ee7f16fe4111fbb63f091cec6
Andrii Morozov <morozov@ztu.edu.ua>
2022-11-08 18:39:18 +0200

    change program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World2!");
-Console.WriteLine("Test message");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Test message");
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
